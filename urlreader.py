4298ef0600467c6c5e0ee29302527b2b51335b00
kenstars <kannanpiedy@gmail.com>
2016-09-24 11:59:40 +0530

    Fixed bugs relating to finding empty rows

--- urlreader.py
+++ urlreader.py
@@ -15,21 +15,24 @@ target = open(output_filename, 'w')
 
 target.write('URL, Title, Meta Description')
 for URL in URL_list[1:]:
-	data = urllib2.urlopen(URL)
-	title = '-'
-	meta_desc = '-'
-	for line in data:
-		if '</head' not in line:
-			if '<title' in line:
-				title = line.split('>')[1].split('<')[0]
-			if '<meta' in line:
-				if 'name="Description"' in line :
-					meta_desc = line.split('content')[1].strip()
-					meta_desc = re.findall(r'"([^"]*)"', meta_desc)[0].strip()
-		else:
-			break
-	target.write(URL+', '+title+', '+meta_desc)
-	target.write("\n")
+	try:
+		data = urllib2.urlopen(URL)
+		title = '-'
+		meta_desc = '-'
+		for line in data:
+			if '</head' not in line:
+				if '<title' in line:
+					title = line.split('>')[1].split('<')[0]
+				if '<meta' in line:
+					if 'name="Description"' in line :
+						meta_desc = line.split('content')[1].strip()
+						meta_desc = re.findall(r'"([^"]*)"', meta_desc)[0].strip()
+			else:
+				break
+		target.write(URL+', '+title+', '+meta_desc)
+		target.write("\n")
+	except:
+		print "Found a row without a valid URL"
 
 
 target.close()
